b0a8839a09af9494d0f2e1e8f7c7bf106ef34c44
Satyajit Shetye <sshetye@Satyajits-MacBook-Pro.local>
2024-11-02 18:13:28 +0530

    project final commit

--- 04-css-styling/src/components/AuthInputs.jsx
+++ 04-css-styling/src/components/AuthInputs.jsx
@@ -1,53 +1,84 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./Input";
+
+const Container = styled.div`
+	&#auth-inputs {
+		width: 100%;
+		max-width: 28rem;
+		padding: 2rem;
+		margin: 0 auto;
+		border-radius: 0.5rem;
+		box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+		background: linear-gradient(180deg, #474232 0%, #28271c 100%);
+		color: white;
+	}
+`;
+
+const Controls = styled.div`
+	&.controls {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		margin-bottom: 1.5rem;
+	}
+`;
+
+const Actions = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	gap: 1rem;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+	const [enteredEmail, setEnteredEmail] = useState("");
+	const [enteredPassword, setEnteredPassword] = useState("");
+	const [submitted, setSubmitted] = useState(false);
 
-  function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
-      setEnteredEmail(value);
-    } else {
-      setEnteredPassword(value);
-    }
-  }
+	function handleInputChange(identifier, value) {
+		if (identifier === "email") {
+			setEnteredEmail(value);
+		} else {
+			setEnteredPassword(value);
+		}
+	}
 
-  function handleLogin() {
-    setSubmitted(true);
-  }
+	function handleLogin() {
+		setSubmitted(true);
+	}
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+	const emailNotValid = submitted && !enteredEmail.includes("@");
+	const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
-  return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
-      </div>
-    </div>
-  );
+	return (
+		<Container id="auth-inputs">
+			<Controls>
+				<Input
+					label="Email"
+					type="text"
+					invalid={emailNotValid}
+					onChange={(event) =>
+						handleInputChange("password", event.target.value)
+					}
+				/>
+				<Input
+					label="Password"
+					type="password"
+					invalid={passwordNotValid}
+					onChange={(event) =>
+						handleInputChange("password", event.target.value)
+					}
+				/>
+			</Controls>
+			<Actions>
+				<Button type="button" className="text-button">
+					Create a new account
+				</Button>
+				<Button className="button" onClick={handleLogin}>
+					Sign In
+				</Button>
+			</Actions>
+		</Container>
+	);
 }
--- 04-css-styling/src/components/Header.jsx
+++ 04-css-styling/src/components/Header.jsx
@@ -1,11 +1,53 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import { styled } from "styled-components";
+
+const HeaderElm = styled.header`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin-top: 2rem;
+	margin-bottom: 2rem;
+
+	@media (min-width: 768px) {
+		margin-bottom: 4rem;
+	}
+`;
+
+const ImageElm = styled.img`
+	object-fit: contain;
+	margin-bottom: 2rem;
+	width: 11rem;
+	height: 11rem;
+`;
+
+const H1Elm = styled.h1`
+	font-size: 1.5rem;
+	font-weight: 600;
+	letter-spacing: 0.4em;
+	text-align: center;
+	text-transform: uppercase;
+	color: #9a3412;
+	font-family: "Pacifico", cursive;
+	margin: 0;
+
+	@media (min-width: 768px) {
+		font-size: 2.25rem;
+	}
+`;
+
+const PElm = styled.p`
+	text-align: center;
+	color: #a39191;
+	margin: 0;
+`;
 
 export default function Header() {
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
-  );
+	return (
+		<HeaderElm>
+			<ImageElm src={logo} alt="A canvas" />
+			<H1Elm>ReactArt</H1Elm>
+			<PElm>A community of artists and art-lovers.</PElm>
+		</HeaderElm>
+	);
 }
